2696be7b0a6d896219aff956dcd865519f11f632
LewiVir <38184679+LewiVir@users.noreply.github.com>
2020-12-17 11:05:25 +0800

    20201217 add lean r4s support

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -16,5 +16,8 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+git rebase --onto e04fd21 37836e0
+git apply $GITHUB_WORKSPACE/patches/0001-Fix-R4S-patch-and-uboot.patch
+
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
